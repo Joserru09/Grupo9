f1fc0cc0d65328f7035b162e1da679285b8e608c
Joserru <jruizesteban99@gmail.com>
2023-12-19 18:59:10 +0100

    añadido la otra pantalla y la logica

--- Grupo9/app/src/main/java/com/upm/es/grupo9/MainActivity.java
+++ Grupo9/app/src/main/java/com/upm/es/grupo9/MainActivity.java
@@ -2,13 +2,40 @@ package com.upm.es.grupo9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText TextUsuario;
+    EditText TextContra;
+    Button buttonLogin;
+    Button buttonRegister;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextUsuario=findViewById(R.id.TextUsuario);
+        TextContra=findViewById(R.id.TextContra);
+        buttonLogin=findViewById(R.id.buttonLogin);
+        buttonRegister=findViewById(R.id.buttonRegister);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarUsuario();
+            }
+        });
+
+    }
+
+    private void validarUsuario(){
+        Intent intent= new Intent(getApplicationContext(),MercadoActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Grupo9/app/src/main/java/com/upm/es/grupo9/MercadoActivity.java
@@ -0,0 +1,14 @@
+package com.upm.es.grupo9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MercadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mercado);
+    }
+}
